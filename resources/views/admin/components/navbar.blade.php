1da9078c12bc447ab5e7cbe3eff7806648fad371
= <=>
2024-05-23 18:39:07 +0700

    change route from default breeze dashboard to admin dashboard

--- resources/views/admin/components/navbar.blade.php
+++ resources/views/admin/components/navbar.blade.php
@@ -28,7 +28,11 @@
                 <li class="nav-item d-flex align-items-center">
                     <a href="./pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
                         <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">Sign In</span>
+                        <span class="d-sm-inline d-none">
+                            @if (Auth::check())
+                                {{Auth::user()->nama}}
+                            @endif
+                        </span>
                     </a>
                 </li>
             </ul>
