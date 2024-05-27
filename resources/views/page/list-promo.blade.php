599559808c1e0df3acf9af16ff03821b29cf80c0
= <=>
2024-05-27 10:48:54 +0700

    up folder

---
+++ resources/views/page/list-promo.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.page.master')
+
+@section('title', 'Welcome to Furniture Max')
+
+@section('css-style')
+    <link rel="stylesheet" href="{{ asset('assets/css/page/list-promo.css') }}">
+@endsection
+
+@section('content')
+
+@endsection
