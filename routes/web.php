aeba92204c8783896ffa31ec1184e0a23d35a988
= <=>
2024-05-27 11:52:19 +0700

    remove profile, foldering blade

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ViewController::class, 'index'])->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::middleware('is_admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/barang', [BarangController::class, 'index'])->name('barang.index');
