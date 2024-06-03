3c995f3c33baa408652661ee77696cd11d2d0479
oliversmk7rpl <oliversmk7rpl@gmail.com>
2024-06-03 02:28:48 +0000

    commit changes for order-complete

--- resources/views/page/order/order-complete.blade.php
+++ resources/views/page/order/order-complete.blade.php
@@ -8,11 +8,11 @@
 
 @section('content')
     <div class="flex justify-center items-center" style="min-height: 81.5vh; margin-top: 13px;">
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col gap-2 items-center">
             <i class="bi bi-check-circle text-sage" style="font-size: 75px"></i>
             <h2 class="fs-2 fw-bold text-sage">PEMBELIAN BERHASIL</h2>
             <p class="mt-2 mb-4 text-center" style="max-width: 500px">Terima kasih telah membeli! orderan kamu akan di proses dalam waktu 4-6 jam. Kamu juga akan menerima notifikasi melalui email saat orderan kamu selesai.</p>
-            <a href="{{ route('order-status') }}" class="btn bg-blue text-white">Cek Status</a>
+            <a href="{{ route('order-status') }}" class="btn bg-blue-500 text-white">Cek Status</a>
             <a href="{{ route('list-produk') }}" class="btn bg-sage text-white">Lanjut Belanja</a>
         </div>
     </div>
