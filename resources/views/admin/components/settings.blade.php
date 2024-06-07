1e9c74f494086d2450c0648f760bc5052718f652
= <=>
2024-06-07 12:23:50 +0700

    fixed sidebar, resetting settings dashboard, responsive for admin

--- resources/views/admin/components/settings.blade.php
+++ resources/views/admin/components/settings.blade.php
@@ -1,12 +1,12 @@
-<div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+<div class="fixed-plugin"id="settingsmenu">
+    <a class="fixed-plugin-button d-none d-lg-block text-dark position-fixed px-3 py-2" id="iconsettings">
         <i class="material-icons py-2">settings</i>
     </a>
     <div class="card shadow-lg">
         <div class="card-header pb-0 pt-3">
             <div class="float-start">
-                <h5 class="mt-3 mb-0">Konfigurasi Material UI</h5>
-                <p>Dashboard Settings</p>
+                <h5 class="mt-3 mb-0">Konfigurasi</h5>
+                <p>Pengaturan Dashboard</p>
             </div>
             <div class="float-end mt-4">
                 <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
@@ -18,7 +18,7 @@
         <hr class="horizontal dark my-1">
         <div class="card-body pt-sm-3 pt-0">
           <!-- Sidebar Backgrounds -->
-            <div>
+            {{-- <div>
                 <h6 class="mb-0">Warna</h6>
             </div>
             <a href="javascript:void(0)" class="switch-trigger background-color">
@@ -30,7 +30,7 @@
                 <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
                 <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
                 </div>
-            </a>
+            </a> --}}
           <!-- Sidenav Type -->
             <div class="mt-3">
                 <h6 class="mb-0">Tema</h6>
@@ -43,12 +43,12 @@
             </div>
             <p class="text-sm d-xl-none d-block mt-2">Kamu Hanya Bisa Mengganti Tema di Tampilan Desktop.</p>
             <!-- Navbar Fixed -->
-            <div class="mt-3 d-flex">
+            {{-- <div class="mt-3 d-flex">
                 <h6 class="mb-0">Selalu Tampilkan Navbar</h6>
                 <div class="form-check form-switch ps-0 ms-auto my-auto">
                 <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
                 </div>
-            </div>
+            </div> --}}
             <hr class="horizontal dark my-3">
             <div class="mt-2 d-flex">
                 <h6 class="mb-0">Light / Dark</h6>
@@ -57,8 +57,8 @@
                 </div>
             </div>
             <hr class="horizontal dark my-sm-4">
-            <a class="btn bg-gradient-info w-100" href="https://www.creative-tim.com/product/material-dashboard-pro">Free Download</a>
-            <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard">View documentation</a>
+            {{-- <a class="btn bg-gradient-info w-100" href="https://www.creative-tim.com/product/material-dashboard-pro">Free Download</a> --}}
+            {{-- <a class="btn btn-outline-dark w-100" id="showsidenav">Toggle Sidebar</a> --}}
         </div>
     </div>
 </div>
--- resources/views/admin/components/sidenav.blade.php
+++ resources/views/admin/components/sidenav.blade.php
@@ -5,8 +5,8 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="  " target="_blank">
-            <img src="{{ asset('/assets/img/logoFM.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold text-white">Furniture Max</span>
+            <img src="{{ asset('/assets/img/LOGOFM2.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            {{-- <span class="ms-1 font-weight-bold text-white">Furniture Max</span> --}}
         </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
@@ -14,7 +14,7 @@
         {{-- SIDENAV --}}
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-white " href="{{ route('home') }}">
+                <a class="nav-link text-white " href="{{ route('dashboard') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">dashboard</i>
                     </div>
@@ -46,14 +46,6 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white " href="#">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">reviews</i>
-                    </div>
-                    <span class="nav-link-text ms-1">Ulasan</span>
-                </a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link text-white " href="{{ route('transaksi.index') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">receipt_long</i>
@@ -77,9 +69,9 @@
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
         <div class="mx-3">
-            <a class="btn btn-outline-primary mt-4 w-100"
+            {{-- <a class="btn btn-outline-primary mt-4 w-100"
                 href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard?ref=sidebarfree"
-                type="button">Dokumentasi Template</a>
+                type="button">Dokumentasi Template</a> --}}
             <form action="{{ route('logout') }}" method="POST">
                 @csrf
                 <button type="submit" class="btn bg-gradient-primary w-100">
